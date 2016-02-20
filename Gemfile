454c1497ebe4ce2d68ffa568de1a6ee308d34067
Thomas Thoren <thomasjthoren@gmail.com>
2016-02-20 09:59:18 -0600

    Adds image scripts and additional HTTPS changes.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 gem 'github-pages'
 gem 'jekyll-gist'
+gem 'jekyll-sitemap'
 
 # Test the build
 group :test do
---
+++ scripts/minify.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+if [ -z $VIRTUAL_ENV ]; then
+  echo Not in virtual environment. Stopping.
+  exit 1
+fi
+
+mkdir -p $PROJECT_DIRECTORY/images/thumbnails
+
+# Copy raw images to temporary directory
+rm -rf $PROJECT_DIRECTORY/images/tmp/
+mkdir -p $PROJECT_DIRECTORY/images/tmp/
+cp $PROJECT_DIRECTORY/images/raw/* \
+   $PROJECT_DIRECTORY/images/tmp
+
+# Scale down if possible. Max width is 678px.
+for photo in $(find $PROJECT_DIRECTORY/images/tmp -type f); do
+  fullfile=$(basename "$photo")  # filename.extension
+  fname=${fullfile%.*}  # filename
+  fileextension="${photo##*.}"  # extension
+
+  # Small width. Limit width to 400px.
+  convert $photo \
+    -interlace plane \
+    -quality 92 \
+    -density 72 \
+    -resize '400>' \
+    -set filename:mysize \
+    "%wx%h" \
+    "$PROJECT_DIRECTORY/images/tmp/$fname-%[filename:mysize].$fileextension"
+
+  # Large width. Limit width to 680px.
+  convert $photo \
+    -interlace plane \
+    -quality 92 \
+    -density 72 \
+    -resize '680>' \
+    -set filename:mysize \
+    "%wx%h" \
+    "$PROJECT_DIRECTORY/images/tmp/$fname-%[filename:mysize].$fileextension"
+
+  # Large width, retina. Limit width to 1360px.
+  convert $photo \
+    -interlace plane \
+    -quality 92 \
+    -density 72 \
+    -resize '1360>' \
+    -set filename:mysize \
+    "%wx%h" \
+    "$PROJECT_DIRECTORY/images/tmp/$fname-%[filename:mysize].$fileextension"
+
+done
+
+# Resample images.
+imageoptim --directory $PROJECT_DIRECTORY/images/tmp/
+
+# Copy into thumbnails directory.
+cp $PROJECT_DIRECTORY/images/tmp/* \
+   $PROJECT_DIRECTORY/images/thumbnails
+
+# Clean up.
+rm -rf $PROJECT_DIRECTORY/images/tmp
